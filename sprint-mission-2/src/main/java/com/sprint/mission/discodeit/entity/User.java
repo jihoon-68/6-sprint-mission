19f245fd0b8d96ba01fe7f5bcaeaf80b114d4fb1
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-19 15:41:23 +0900

    User Getter,update 매서드 추가

--- sprint-mission-2/src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ sprint-mission-2/src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.UUID;
 
 public class User {
-    private UUID id;
+    private final UUID id;
     private String username;
-    private int age;
+    private final int age;
     private String email;
     private List<User> friends;
     private List<Channel>  channels;
-    private Long created;
+    private final Long created;
     private Long updated;
 
     public User(String username, int age, String email) {
@@ -24,5 +24,39 @@ public class User {
         this.channels = new ArrayList<>();
         this.created = System.currentTimeMillis();
         this.updated = System.currentTimeMillis();
+    }
+    //유저에 변경이 생기면 업데이트 시간 변경
+    public void updatedChannel() {
+        this.updated = System.currentTimeMillis();
+    }
+
+    //Getter 생성
+    public UUID getUserId() {return this.id;}
+    public String getUsername() {return this.username;}
+    public int getAge() {return this.age;}
+    public String getEmail() {return this.email;}
+    public List<User> getFriends() {return this.friends;}
+    public List<Channel> getChannels() {return this.channels;}
+    public Long getCreated() {return this.created;}
+    public Long getUpdated() {return this.updated;}
+
+    //update
+    public void updateedUsername(String username) {
+        this.username = username;
+        updatedChannel();
+    }
+
+    public  void updateEmail(String email) {
+        this.email = email;
+        updatedChannel();
+    }
+
+    //유져 본연에 속성이 변경 시에만 업데이트 갱신
+    public void updatedFriends(List<User> friends) {
+        this.friends = friends;
+    }
+
+    public void updatedChannels(List<Channel> channels) {
+        this.channels = channels;
     }
 }
