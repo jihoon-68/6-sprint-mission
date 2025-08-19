bc9f168574312dd874631ed4041ea7adc314b485
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-19 15:05:54 +0900

    Channel 속성, 생성자 추가

--- sprint-mission-2/src/main/java/com/sprint/mission/discodeit/entity/Channel.java
+++ sprint-mission-2/src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -1,4 +1,25 @@
 package com.sprint.mission.discodeit.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Channel {
+    private UUID id;
+    private String channelName;
+    private User root;
+    private List<User> users;
+    private List<Message> messages;
+    private Long created;
+    private Long updated;
+
+    public Channel(String channelName, User root) {
+        this.id = UUID.randomUUID();
+        this.channelName = channelName;
+        this.root = root;
+        this.users = new ArrayList<>();
+        this.messages = new ArrayList<>();
+        this.created = System.currentTimeMillis();
+        this.updated = System.currentTimeMillis();
+    }
 }
