12013c5c167430f9841ccb379ec93ea4c94da3d2
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-19 15:07:02 +0900

    Message 속성, 생성자 추가

--- sprint-mission-2/src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ sprint-mission-2/src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -1,4 +1,22 @@
 package com.sprint.mission.discodeit.entity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 public class Message {
+    private UUID id;
+    private User sender;
+    private String text;
+    private Long created;
+    private Long updated;
+
+    public Message(User sender, String text) {
+        this.id = UUID.randomUUID();
+        this.sender = sender;
+        this.text = text;
+        this.created = System.currentTimeMillis();
+        this.updated = System.currentTimeMillis();
+    }
+
 }
