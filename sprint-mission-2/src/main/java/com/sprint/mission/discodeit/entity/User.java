e513ee5335c9bbbd13796e8ccdb932979cc71c5f
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-19 15:07:32 +0900

    User 속성, 생성자 추가

--- sprint-mission-2/src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ sprint-mission-2/src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,4 +1,28 @@
 package com.sprint.mission.discodeit.entity;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
 public class User {
+    private UUID id;
+    private String username;
+    private int age;
+    private String email;
+    private List<User> friends;
+    private List<Channel>  channels;
+    private Long created;
+    private Long updated;
+
+    public User(String username, int age, String email) {
+        this.id = UUID.randomUUID();
+        this.username = username;
+        this.age = age;
+        this.email = email;
+        this.friends = new ArrayList<>();
+        this.channels = new ArrayList<>();
+        this.created = System.currentTimeMillis();
+        this.updated = System.currentTimeMillis();
+    }
 }
