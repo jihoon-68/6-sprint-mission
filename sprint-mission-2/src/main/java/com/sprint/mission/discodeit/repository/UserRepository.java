529478e5fba0d0fff8f5bb6548440ca6be6f51e6
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-21 18:42:59 +0900

    인터 페이스 파일 생성 및 선언

---
+++ sprint-mission-2/src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserRepository {
+
+    //기존 CRUD
+    public void createUser(String username, int age , String email);
+    public User findUserById(UUID id);
+    public User findUserByUserEmail(String userEmail);
+    public List<User> findAllUsers();
+    public void updateUser(User user);
+    public void deleteUser(UUID id);
+
+    //유저에 서버 추가
+    public void addUserToChannel(Channel channel, User user);
+    public void removeUserFromChannel(Channel channel, User user);
+
+    //친구 추가
+    public void addUserToFriend(User user, UUID id);
+    public void removeUserFromFriend(User user, UUID id);
+}
