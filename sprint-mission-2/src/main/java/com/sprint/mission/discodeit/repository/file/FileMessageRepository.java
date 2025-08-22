d929f0337c84008764f629e8547f85e6a0c3aae2
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-22 18:30:18 +0900

    인터페이스 구현체 구현

--- sprint-mission-2/src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
+++ sprint-mission-2/src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -1,4 +1,49 @@
 package com.sprint.mission.discodeit.repository.file;
 
-public class FileMessageRepository {
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.UUID;
+
+public class FileMessageRepository implements MessageRepository {
+    private static final Path directory = Paths.get("/Users/mac/IdeaProjects/6-sprint-mission/sprint-mission-2/src/main/resources/ChannelDate");
+    private static final FileEdit instance = new  FileEdit();
+
+    private Path filePaths(Message message) {
+
+        return directory.resolve(message.getMessageId().toString() + ".ser");
+    }
+    public FileMessageRepository() {
+        instance.init(directory);
+    }
+
+    public Message createMessage(User sender, String message) {
+        Message newMessage = new Message(sender, message);
+        instance.save(filePaths(newMessage), newMessage);
+        return newMessage;
+    }
+
+    public Message findMessageById(UUID id) {
+        List<Message> messageList = instance.load(directory);
+        return messageList.stream()
+                .filter(message -> message.getMessageId().equals(id))
+                .toList()
+                .get(0);
+    }
+
+    public List<Message> findAllMessages() {
+        return instance.load(directory);
+    }
+
+    public void updateMessage(Message message) {
+        instance.save(filePaths(message), message);
+    }
+
+    public void deleteMessage(UUID id) {
+        instance.delete(filePaths(findMessageById(id)));
+    }
 }
