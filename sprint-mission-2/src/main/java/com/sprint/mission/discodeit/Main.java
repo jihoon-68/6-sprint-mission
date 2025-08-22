adfff215ca7e960a94b346e28b182879096ddb2c
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-22 18:40:47 +0900

    Basic*Service 구현체를 활용하여 테스트 코드 작성

--- sprint-mission-2/src/main/java/com/sprint/mission/discodeit/Main.java
+++ sprint-mission-2/src/main/java/com/sprint/mission/discodeit/Main.java
@@ -3,18 +3,70 @@ package com.sprint.mission.discodeit;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.service.file.FileChannelService;
-import com.sprint.mission.discodeit.service.file.FileMessageService;
-import com.sprint.mission.discodeit.service.file.FileUserService;
-import com.sprint.mission.discodeit.service.jcf.JCFChannel;
-import com.sprint.mission.discodeit.service.jcf.JCFMessage;
-import com.sprint.mission.discodeit.service.jcf.JCFUser;
-
+import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
+import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
+import com.sprint.mission.discodeit.repository.file.FileUserRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFChannelRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFMessageRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
+import com.sprint.mission.discodeit.service.basic.BasicChannelService;
+import com.sprint.mission.discodeit.service.basic.BasicMessageService;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
 
 
 public class Main {
 
+    static User setupUser(BasicUserService basicUserService) {
+        return basicUserService.create("woody",20 ,"woody@codeit.com");
+    }
+
+    static Channel setupChannel(BasicChannelService basicChannelService,User user) {
+        Channel channel = basicChannelService.create("공지", user);
+        return channel;
+    }
+
+    static void messageCreateTest(BasicMessageService basicMessageService, Channel channel, User author) {
+        Message message = basicMessageService.create(channel,author,"안녕하세요");
+        System.out.println("메시지 생성: " + message.getMessageId());
+    }
+
     public static void main(String[] args) {
+
+
+        //JCF 저장 방식
+        JCFUserRepository jcfUserRepository = new JCFUserRepository();
+        JCFChannelRepository jcfChannelRepository = new JCFChannelRepository();
+        JCFMessageRepository jcfMessageRepository = new JCFMessageRepository();
+
+        BasicUserService JCFbasicUserService = new BasicUserService(jcfUserRepository);
+        BasicChannelService JCFbasicChannelService = new BasicChannelService(jcfChannelRepository);
+        BasicMessageService JCFbasicMessageService = new BasicMessageService(jcfMessageRepository,jcfChannelRepository);
+
+
+        User JCFUUser = setupUser(JCFbasicUserService);
+        Channel JCFUChannel = setupChannel(JCFbasicChannelService,JCFUUser);
+        // 테스트
+        messageCreateTest(JCFbasicMessageService, JCFUChannel, JCFUUser);
+
+
+        //File 저장 방식
+        FileUserRepository fileUserRepository = new FileUserRepository();
+        FileMessageRepository fileMessageRepository = new FileMessageRepository();
+        FileChannelRepository fileChannelRepository = new FileChannelRepository();
+
+        BasicUserService fileBasicUserService = new BasicUserService(fileUserRepository);
+        BasicChannelService fileBasicChannelService = new BasicChannelService(fileChannelRepository);
+        BasicMessageService fileBasicMessageService = new BasicMessageService(fileMessageRepository, fileChannelRepository);
+
+
+        User FileUser = setupUser(fileBasicUserService);
+        Channel FileChannel = setupChannel(fileBasicChannelService,FileUser);
+        // 테스트
+        messageCreateTest(fileBasicMessageService, FileChannel, FileUser);
+
+
+
+
         /* JCF 저장형식 구현방식
         JCFUser jcfUser = new JCFUser();
         JCFMessage jcfMessage = new JCFMessage();
@@ -158,7 +210,7 @@ public class Main {
         System.out.println(jcfChannel.findChannelById(channel1.getChannelId()).getChannelMessages());
         System.out.println(jcfChannel.findChannelById(channel2.getChannelId()).getChannelMessages());
         System.out.println("================================================");
-         */
+
 
 
         FileUserService fileUserService = new FileUserService();
@@ -262,6 +314,8 @@ public class Main {
         System.out.println("================메시지 삭제=================");
         fileMessageService.deleteMessage(newMessage.getMessageId());
         System.out.println(fileMessageService.findAllMessages());
+        */
+
 
     }
 
