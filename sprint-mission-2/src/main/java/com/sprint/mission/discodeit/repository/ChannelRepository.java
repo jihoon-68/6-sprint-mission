6720bd55e2cd8d390af98376381ae4c19a19f8d0
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-22 17:54:22 +0900

    create 메서드 매개변수 root 형 String -> User 변경, 체널에 메시지 추가/삭제 메서드 추가

--- sprint-mission-2/src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
+++ sprint-mission-2/src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
@@ -8,18 +8,20 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ChannelRepository {
-    public Channel createChannel(String name, String root);
+    public Channel createChannel(String name, User root);
     public Channel findChannelById(UUID id);
     public List<Channel> findAllChannels();
     public void updateChannel(Channel channel);
     public void deleteChannel(UUID id);
 
-    //체널에 유저 추가
-    public void addUserToChannel(Channel channel, User user);
-    public void removeUserFromChannel(Channel channel, User user);
-
     //체널에 메시지 추가
     public void addMessageToChannel(Channel channel, Message message);
     public void removeMessageFromChannel(Channel channel, Message message);
 
+    /*
+    //구현 순위 낮음
+    //체널에 유저 추가
+    public void addUserToChannel(Channel channel, User user);
+    public void removeUserFromChannel(Channel channel, User user);
+    */
 }
