2e0d5170b73f7e1b2fa96f0fbb0b784ef23a5205
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-22 17:34:25 +0900

    create 메서드 senderName 매개변수 형 String -> User 변경, updateMessage 메서드 Message -> void 변경

--- sprint-mission-2/src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ sprint-mission-2/src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -7,9 +7,9 @@ import java.util.List;
 import java.util.UUID;
 
 public interface MessageService {
-    public Message createMessage(String senderName, String message);
+    public Message createMessage(User sender, String message);
     public Message findMessageById(UUID id);
     public List<Message> findAllMessages();
-    public Message updateMessage(Message message);
+    public void updateMessage(Message message);
     public void deleteMessage(UUID id);
 }
