090ade1dd6916783d5205687d7cf932647a059b2
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-02 00:36:36 +0900

    BaseEntity추상 클래스를 만들어서 로직을 위로 올리기

--- sprint-mission-2/src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ sprint-mission-2/src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -3,7 +3,7 @@ package com.sprint.mission.discodeit.entity;
 import java.io.Serializable;
 import java.util.UUID;
 
-public class Message implements Serializable {
+public class Message extends BaseEntity implements Serializable {
     private static final long serialVersionUID = 2L;
     private final UUID id;
     private final User sender;
@@ -15,7 +15,7 @@ public class Message implements Serializable {
         this.id = UUID.randomUUID();
         this.sender = sender;
         this.text = text;
-        this.created = System.currentTimeMillis();
+        this.created = setTime();
     }
 
     public UUID getMessageId() {return this.id;}
@@ -26,7 +26,7 @@ public class Message implements Serializable {
 
     public void updateMessage(String text) {
         this.text = text;
-        this.updated = System.currentTimeMillis();
+        this.updated = setTime();
     }
 
     public String toString(){
@@ -34,6 +34,7 @@ public class Message implements Serializable {
                 "메시지 ID: " + this.id + "\n" +
                 "메시지 발신자: " + this.sender.getUsername() + "\n" +
                 "메시지 내용: " + this.text + "\n" +
-                "메시지 샹성일자: " + this.created + "\n";
+                "메시지 생성일자: " + this.created + "\n" +
+                "메시지 수정일자: " + this.updated + "\n";
     }
 }
