009f547339af19269966f8ff559778bce2728f5e
Hyunwook <hjhsha827@gmail.com>
2025-09-04 13:06:59 +0900

    feat: BinaryContent

--- 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -1,4 +1,27 @@
 package com.sprint.mission.discodeit.entity;
 
-public class BinaryContent {
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+public class BinaryContent extends Common {
+    private static final long serialVersionUID = 1L;
+
+    private UUID profileId;
+    private UUID messageId;
+    private String attatchmentUrl;
+
+    public BinaryContent(UUID profileId, UUID messageId, String attatchmentUrl) {
+        this.profileId = profileId;
+        this.messageId = messageId;
+        this.attatchmentUrl = attatchmentUrl;
+    }
+
+    public void changeProfileImage(String attatchmentUrl) {
+        if (attatchmentUrl != null && !attatchmentUrl.equals(this.attatchmentUrl)) {
+            this.attatchmentUrl = attatchmentUrl;
+        }
+    }
 }
--- 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFBinaryContentRepository.java
+++ 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFBinaryContentRepository.java
@@ -1,4 +1,59 @@
 package com.sprint.mission.discodeit.repository.jcf;
 
-public class JCFBinaryContentRepository {
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.repository.BinaryContentRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+@ConditionalOnProperty(
+        prefix = "discodeit.repository",
+        name = "type",
+        havingValue = "jcf"
+)
+public class JCFBinaryContentRepository implements BinaryContentRepository {
+
+    private final Map<UUID, BinaryContent> map = new HashMap<>();
+
+    @Override
+    public BinaryContent save(BinaryContent binaryContent) {
+        map.put(binaryContent.getId(), binaryContent);
+        return binaryContent;
+    }
+
+    @Override
+    public Optional<BinaryContent> find(UUID id) {
+        return Optional.ofNullable(map.get(id));
+    }
+
+    @Override
+    public Optional<BinaryContent> findByUserId(UUID userId) {
+        return map.entrySet()
+                .stream()
+                .filter(x -> x.getValue().getProfileId().equals(userId))
+                .map(Map.Entry::getValue)
+                .findFirst();
+    }
+
+    @Override
+    public Map<UUID, BinaryContent> findAll() {
+        return Map.of();
+    }
+
+    @Override
+    public void deleteByUserId(UUID userId) {
+        map.entrySet().removeIf(x -> x.getValue().getProfileId().equals(userId));
+    }
+
+    @Override
+    public void deleteByMessageId(UUID messageId) {
+        map.entrySet().removeIf(x -> x.getValue().getMessageId().equals(messageId));
+    }
+
+    @Override
+    public void delete(UUID id) {
+        map.remove(id);
+    }
 }
