db29bc374718d03267840c747e21d6e593e0d7df
hiingjan <phjung1216@naver.com>
2025-09-09 09:51:39 +0900

    Change Common.java

--- CodeIt_SprintMission2/src/main/java/com/sprint/mission/discodeit/entity/Common.java
+++ CodeIt_SprintMission2/src/main/java/com/sprint/mission/discodeit/entity/Common.java
@@ -1,13 +1,15 @@
 package com.sprint.mission.discodeit.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 public class Common implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private UUID id;
-    private Long createdAt;
+    private final UUID id; // 변경되면 안됨
+    private final Long createdAt; // 변경되면 안됨
     private Long updateAt;
 
     public Common(){
@@ -29,8 +31,19 @@ public class Common implements Serializable {
         return updateAt;
     }
 
-    public Long setUpdateAt(Long updateAt){
-        return this.updateAt = updateAt;
+    public Long setUpdateAt(){
+        return this.updateAt = System.currentTimeMillis();
     }
+
+    // User, Channel, Message의 필드 하나씩 변곁
+    protected <T> void setIfChanged(T currentValue, T newValue, Consumer<T> assign, String fieldName) {
+        if (Objects.equals(currentValue, newValue)){
+            throw new IllegalArgumentException(fieldName + " is not changed");
+        }
+        assign.accept(newValue);
+        this.setUpdateAt();
+
+    }
+
 
 }
