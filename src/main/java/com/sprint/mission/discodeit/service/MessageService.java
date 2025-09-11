70b5cadf61857f9be70f7f3b2ba2084a6179f10c
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-11 11:48:04 +0900

    메소드 파라미터 구룹화, findAll 메소드 특정 채널기준으로 메세지 전부 불러오로 변경으로 메소드 findAllByChannelId 명 변경

--- src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -1,15 +1,16 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.DTO.Message.CreateMessageDTO;
+import com.sprint.mission.discodeit.DTO.Message.UpdateMessageDTO;
 import com.sprint.mission.discodeit.entity.Message;
-import com.sprint.mission.discodeit.entity.User;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface MessageService {
-    Message create(UUID channel, UUID user, String content);
+    Message create(CreateMessageDTO createMessageDTO);
     Message find(UUID id);
-    List<Message> findAll();
-    void update(Message message);
+    List<Message> findAllByChannelId(UUID channelId);
+    void update(UpdateMessageDTO updateMessageDTO);
     void delete(UUID id);
 }
