86396b9bec1db738e79a0d48f4148eebb25a3d90
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-11 14:13:34 +0900

    creat 메서드 유저 생성시 이름,이메일 중복 확인 조건, userRepository 같이 생성 추가 / find,findAll 메서드 유저데이터 반환할떄 userRepository 같이 반환으로 변경 / delete user 삭제시 binaryContent, userStatus 도 같이 삭제로 변경

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -1,45 +1,85 @@
 package com.sprint.mission.discodeit.service.basic;
 
-import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.DTO.User.CreateUserDTO;
+import com.sprint.mission.discodeit.DTO.User.FindUserDTO;
+import com.sprint.mission.discodeit.DTO.User.UpdateUserDTO;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sun.jdi.request.DuplicateRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BasicUserService implements UserService{
     private final UserRepository userRepository;
+    private final UserStatusRepository userStatusRepository;
+    private final BinaryContentRepository binaryContentRepository;
 
     @Override
-    public User create(String username, int age, String email){
+    public User create(CreateUserDTO createUserDTO){
+        List<User> users = userRepository.findAll();
+        boolean nameIsDuplicate = users.stream()
+                .anyMatch(user -> user.getUsername().equals(createUserDTO.userName()));
 
-        return userRepository.save(new User(username,age,email));
+        if(nameIsDuplicate){
+            throw new DuplicateRequestException("Username already exists");
+        }
+
+        boolean emailIsDuplicate = users.stream()
+                .anyMatch(user -> user.getEmail().equals(createUserDTO.userName()));
+
+        if(emailIsDuplicate){
+            throw new DuplicateRequestException("Email already exists");
+        }
+
+        User user =userRepository.save(new User(createUserDTO));
+        userStatusRepository.save(new UserStatus(user.getId()));
+        return user;
     }
 
     @Override
-    public User find(UUID id) {
-        return userRepository.findById(id).orElseThrow(() -> new NullPointerException("User not found"));
+    public FindUserDTO find(UUID id) {
+        User user = userRepository.findById(id).orElseThrow(()->new NullPointerException("User not found"));
+        UserStatus userStatus = userStatusRepository.findByUserId(user.getId()).orElseThrow(()->new NullPointerException("UserStatus not found"));
+        return new FindUserDTO(user,userStatus);
     }
 
     @Override
-    public List<User> findAll() {
-        return List.copyOf(userRepository.findAll());
+    public List<FindUserDTO> findAll() {
+        List<User> users = userRepository.findAll();
+        List<UserStatus> userStatuses = userStatusRepository.findAll();
+        Map<UUID, UserStatus> userStatusMap = userStatuses.stream()
+                .collect(Collectors.toMap(UserStatus::getUserId, userStatus->userStatus));
+        return users.stream()
+                .map(user -> new FindUserDTO(user,userStatusMap.get(user.getId())))
+                .toList();
     }
 
     @Override
-    public void update(User user) {
-        User userUpdate = find(user.getId());
-        userUpdate.update(user.getUsername(),userUpdate.getEmail(),user.getPassword());
-        userRepository.save(userUpdate);
+    public void update(UpdateUserDTO updateUserDTO) {
+        User user = userRepository.findById(updateUserDTO.id()).orElseThrow(()-> new NullPointerException("user not found"));
+        user.update(updateUserDTO);
+        userRepository.save(user);
     }
 
     @Override
     public void delete(UUID id) {
+        UserStatus userStatus = userStatusRepository.findByUserId(id).orElseThrow(()-> new NullPointerException("UserStatus not found"));
+        User user = userRepository.findById(id).orElseThrow(()-> new NullPointerException("User not found"));
+
+        binaryContentRepository.deleteById(user.getProfileId());
+        userStatusRepository.deleteById(userStatus.getId());
         userRepository.deleteById(id);
     }
 }
