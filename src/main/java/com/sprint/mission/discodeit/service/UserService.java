c0e1cab460f17f722b6ae49ef459412dc790cc7b
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-18 13:23:06 +0900

    파일 받는 MultipartFile 변수 추가

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -4,15 +4,16 @@ import com.sprint.mission.discodeit.DTO.User.CreateUserDTO;
 import com.sprint.mission.discodeit.DTO.User.FindUserDTO;
 import com.sprint.mission.discodeit.DTO.User.UpdateUserDTO;
 import com.sprint.mission.discodeit.entity.User;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
-      User create(CreateUserDTO createUserDTO);
+      User create(List<MultipartFile> multipartFile,CreateUserDTO createUserDTO);
       FindUserDTO find(UUID id);
       FindUserDTO findEmail(String email);
       List<FindUserDTO> findAll();
-      void update(UpdateUserDTO updateUserDTO);
+      void update(List<MultipartFile> multipartFile ,UpdateUserDTO updateUserDTO);
       void delete(UUID id);
 }
