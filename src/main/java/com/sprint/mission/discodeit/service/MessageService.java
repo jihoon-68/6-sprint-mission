890f28e3f578167fdc60d17042c21c4517c2c09c
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-18 13:22:18 +0900

    파일 받는 MultipartFile 변수 추가

--- src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -3,14 +3,15 @@ package com.sprint.mission.discodeit.service;
 import com.sprint.mission.discodeit.DTO.Message.CreateMessageDTO;
 import com.sprint.mission.discodeit.DTO.Message.UpdateMessageDTO;
 import com.sprint.mission.discodeit.entity.Message;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface MessageService {
-    Message create(CreateMessageDTO createMessageDTO);
+    Message create(List<MultipartFile> multipartFiles, CreateMessageDTO createMessageDTO);
     Message find(UUID id);
     List<Message> findAllByChannelId(UUID channelId);
-    void update(UpdateMessageDTO updateMessageDTO);
+    void update(List<MultipartFile> multipartFiles, UpdateMessageDTO updateMessageDTO);
     void delete(UUID id);
 }
