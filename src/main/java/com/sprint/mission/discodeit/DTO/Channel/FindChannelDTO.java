192144cd965f6cbb22aa09a6d1c93c7842b229a4
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-18 11:30:56 +0900

    createPublicChannelDto 매소드 추가

--- src/main/java/com/sprint/mission/discodeit/DTO/Channel/FindChannelDTO.java
+++ src/main/java/com/sprint/mission/discodeit/DTO/Channel/FindChannelDTO.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.DTO.Channel;
 
+import com.sprint.mission.discodeit.DTO.User.FindUserDTO;
 import com.sprint.mission.discodeit.entity.Channel;
 
 import java.time.Instant;
@@ -10,4 +11,12 @@ public record FindChannelDTO(
         Channel channel,
         Instant recentMessage,
         UUID userId
-) {}
+) {
+    static public FindChannelDTO createPublicChannelDto(Channel channel, Instant recentMessage) {
+        return new FindChannelDTO(
+                channel,
+                recentMessage,
+                null
+        );
+    }
+}
