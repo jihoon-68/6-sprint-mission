ed620dcaa558a63cd839a7586ec139dd1fed448c
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-18 13:16:47 +0900

    CreateBinaryContentDTO -> CreateBinaryContentUserDTO 변경

--- src/main/java/com/sprint/mission/discodeit/service/BinaryContentService.java
+++ src/main/java/com/sprint/mission/discodeit/service/BinaryContentService.java
@@ -1,13 +1,13 @@
 package com.sprint.mission.discodeit.service;
 
-import com.sprint.mission.discodeit.DTO.BinaryContent.CreateBinaryContentDTO;
+import com.sprint.mission.discodeit.DTO.BinaryContent.CreateBinaryContentUserDTO;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface BinaryContentService {
-    BinaryContent  create(CreateBinaryContentDTO createBinaryContentDTO);
+    BinaryContent  create(CreateBinaryContentUserDTO CreateBinaryContentUserDTO);
     BinaryContent findById(UUID id);
     List<BinaryContent> findAll();
     void deleteById(UUID id);
