8afa3fa5f2906e06dfb08d74264026c9b4df5cdd
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-18 10:45:47 +0900

    README 이름

--- src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.entity;
-
-import java.time.Instant;
-
-public abstract class BaseEntity {
-    public Instant setTime() {
-        return Instant.now();
-    }
-}
