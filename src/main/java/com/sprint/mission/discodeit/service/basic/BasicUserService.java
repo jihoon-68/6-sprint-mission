545494f495310340bb5d78d1d6725f6a0977213c
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-18 13:45:28 +0900

    예외처리 변경, User 생산자 매개 변수 변경으로 인한 수정

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -3,62 +3,83 @@ package com.sprint.mission.discodeit.service.basic;
 import com.sprint.mission.discodeit.DTO.User.CreateUserDTO;
 import com.sprint.mission.discodeit.DTO.User.FindUserDTO;
 import com.sprint.mission.discodeit.DTO.User.UpdateUserDTO;
+import com.sprint.mission.discodeit.utilit.FileUpload;
+import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.UserService;
-import com.sun.jdi.request.DuplicateRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+
 public class BasicUserService implements UserService{
     private final UserRepository userRepository;
     private final UserStatusRepository userStatusRepository;
     private final BinaryContentRepository binaryContentRepository;
+    private final FileUpload fileUpload;
 
     @Override
-    public User create(CreateUserDTO createUserDTO){
-        List<User> users = userRepository.findAll();
-        boolean nameIsDuplicate = users.stream()
-                .anyMatch(user -> user.getUsername().equals(createUserDTO.userName()));
+    public User create(List<MultipartFile> multipartFile, CreateUserDTO createUserDTO){
 
-        if(nameIsDuplicate){
-            throw new DuplicateRequestException("Username already exists");
+        //유저 중복 확인
+        if(isDuplicate(createUserDTO.userName(), createUserDTO.email())){
+            throw new DuplicateFormatFlagsException("Username already exists");
         }
 
-        boolean emailIsDuplicate = users.stream()
-                .anyMatch(user -> user.getEmail().equals(createUserDTO.userName()));
+        //유저 중복 확인후 유저 생성
+        User user = userRepository.save(new User(
+                createUserDTO.userName(),
+                createUserDTO.age(),
+                createUserDTO.email(),
+                createUserDTO.password()
+                )
+        );
 
-        if(emailIsDuplicate){
-            throw new DuplicateRequestException("Email already exists");
+        //파일 있으면 파일 생성
+        if(!multipartFile.isEmpty()){
+            List<Path> fileDTOS = fileUpload.upload(multipartFile).stream()
+                    .map(fileDTO -> fileDTO.file().toPath())
+                    .toList();
+
+            BinaryContent binaryContent = binaryContentRepository.save(
+                    new BinaryContent(user.getId(),fileDTOS.get(0).toString()));
+
+            user.update(UpdateUserDTO.getFileInput(binaryContent.getId()));
         }
 
-        User user = userRepository.save(new User(createUserDTO));
+        //유저 접속 샹태 생성
         userStatusRepository.save(new UserStatus(user.getId()));
-        return user;
+
+        return userRepository.save(user);
     }
 
     @Override
     public FindUserDTO find(UUID id) {
-        User user = userRepository.findById(id).orElseThrow(()->new NullPointerException("User not found"));
-        UserStatus userStatus = userStatusRepository.findByUserId(user.getId()).orElseThrow(()->new NullPointerException("UserStatus not found"));
+        User user = userRepository.findById(id)
+                .orElseThrow(()->new NoSuchElementException("User not found"));
+        UserStatus userStatus = userStatusRepository.findByUserId(user.getId())
+                .orElseThrow(()->new NoSuchElementException("UserStatus not found"));
         return new FindUserDTO(user,userStatus);
     }
 
     @Override
     public FindUserDTO findEmail(String email) {
-        User user = userRepository.findByEmail(email).orElseThrow(()->new NullPointerException("User not found"));
-        UserStatus userStatus = userStatusRepository.findByUserId(user.getId()).orElseThrow(()->new NullPointerException("UserStatus not found"));
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(()->new NoSuchElementException("User not found"));
+        UserStatus userStatus = userStatusRepository.findByUserId(user.getId())
+                .orElseThrow(()->new NoSuchElementException("UserStatus not found"));
         return new FindUserDTO(user,userStatus);
     }
 
@@ -67,7 +88,7 @@ public class BasicUserService implements UserService{
         List<User> users = userRepository.findAll();
         List<UserStatus> userStatuses = userStatusRepository.findAll();
         Map<UUID, UserStatus> userStatusMap = userStatuses.stream()
-                .collect(Collectors.toMap(UserStatus::getUserId, userStatus->userStatus));
+                .collect(Collectors.toMap(UserStatus::getUserId, Function.identity()));
 
         return users.stream()
                 .map(user -> new FindUserDTO(user,userStatusMap.get(user.getId())))
@@ -75,19 +96,47 @@ public class BasicUserService implements UserService{
     }
 
     @Override
-    public void update(UpdateUserDTO updateUserDTO) {
-        User user = userRepository.findById(updateUserDTO.id()).orElseThrow(()-> new NullPointerException("user not found"));
+    public void update(List<MultipartFile> multipartFile,UpdateUserDTO updateUserDTO) {
+
+        //유저 중복 확인
+        if(isDuplicate(updateUserDTO.userName(), updateUserDTO.email())){
+            throw new DuplicateFormatFlagsException("Username already exists");
+        }
+
+        User user = userRepository.findById(updateUserDTO.id()).orElseThrow(()-> new NoSuchElementException("user not found"));
+        if(!multipartFile.isEmpty()){
+            File profile = fileUpload.upload(multipartFile).get(0).file();
+            UUID profileId = binaryContentRepository.save(
+                    new BinaryContent(updateUserDTO.id(),profile.toString())).getId();
+
+            user.update(new UpdateUserDTO(
+                    updateUserDTO.id(),
+                    updateUserDTO.userName(),
+                    updateUserDTO.email(),
+                    profileId,
+                    updateUserDTO.password())
+            );
+            userRepository.save(user);
+            return;
+        }
+
         user.update(updateUserDTO);
         userRepository.save(user);
     }
 
     @Override
     public void delete(UUID id) {
-        UserStatus userStatus = userStatusRepository.findByUserId(id).orElseThrow(()-> new NullPointerException("UserStatus not found"));
-        User user = userRepository.findById(id).orElseThrow(()-> new NullPointerException("User not found"));
+        UserStatus userStatus = userStatusRepository.findByUserId(id).orElseThrow(()-> new NoSuchElementException("UserStatus not found"));
+        User user = userRepository.findById(id).orElseThrow(()-> new NoSuchElementException("User not found"));
 
         binaryContentRepository.deleteById(user.getProfileId());
         userStatusRepository.deleteById(userStatus.getId());
         userRepository.deleteById(id);
+    }
+
+    public boolean isDuplicate(String name,String email) {
+        List<User> users = userRepository.findAll();
+        return users.stream().noneMatch(user -> user.getUsername().equals(name)) ||
+                users.stream().noneMatch(user -> user.getEmail().equals(email));
     }
 }
