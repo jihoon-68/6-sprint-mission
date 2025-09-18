bb255ea3730f1627f7ea2252e6b3360901e1d1f4
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-18 11:27:09 +0900

    공개 채널에 생산시에 생성자에서 지정 했으로 제거

--- src/main/java/com/sprint/mission/discodeit/DTO/Channel/CreatePublicChannelDTO.java
+++ src/main/java/com/sprint/mission/discodeit/DTO/Channel/CreatePublicChannelDTO.java
@@ -1,11 +1,8 @@
 package com.sprint.mission.discodeit.DTO.Channel;
 
-import com.sprint.mission.discodeit.Enum.ChannelType;
-
 public record CreatePublicChannelDTO(
         String channelName,
-        String description,
-        ChannelType channelType
+        String description
 ) {
 
 }
