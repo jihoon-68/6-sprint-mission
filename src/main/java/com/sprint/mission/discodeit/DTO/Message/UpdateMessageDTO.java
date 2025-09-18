292df9e28d00ccdc9c21eb6aef54f6ce51a533c8
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-18 11:43:55 +0900

    파일들 id로 변경

--- src/main/java/com/sprint/mission/discodeit/DTO/Message/UpdateMessageDTO.java
+++ src/main/java/com/sprint/mission/discodeit/DTO/Message/UpdateMessageDTO.java
@@ -1,8 +1,10 @@
 package com.sprint.mission.discodeit.DTO.Message;
 
+import java.util.List;
 import java.util.UUID;
 
 public record UpdateMessageDTO(
         UUID id,
-        String Content
+        String Content,
+        List<UUID> attachmentIds
 ) {}
