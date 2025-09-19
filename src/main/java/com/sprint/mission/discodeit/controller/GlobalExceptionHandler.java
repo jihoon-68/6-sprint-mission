b591053971c4beef2e0a71318a5ea812864b43e9
3Park <lovehyunju7@naver.com>
2025-09-19 13:50:43 +0900

    ExceptionHandler 항목 추가

--- src/main/java/com/sprint/mission/discodeit/controller/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/controller/GlobalExceptionHandler.java
@@ -4,20 +4,42 @@ import ch.qos.logback.core.model.Model;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleValidationException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        saveLog(ex, request);
+        return Map.of("message", ex.getMessage());
+    }
+
+    @ExceptionHandler(value = NoSuchElementException.class )
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, Object> handleNoSuchElementException(NoSuchElementException ex, HttpServletRequest request) {
+        saveLog(ex, request);
+        return Map.of("message", ex.getMessage());
+    }
+
     @ExceptionHandler(value = Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String,Object> handleException(Exception e, HttpServletRequest request) {
-        log.error("Error message : {}, url : {}, trace : {}", e.getMessage(), request.getRequestURL().toString(), e);
 
+        saveLog(e, request);
         return Map.of("message", e.getMessage());
+    }
+
+    private void saveLog(Exception e, HttpServletRequest request)
+    {
+        log.error("Error message : {}, url : {}, trace : {}", e.getMessage(), request.getRequestURL().toString(), e);
     }
 }
