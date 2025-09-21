18ea5cba7c5711d10742925c9b35e8e93a047e90
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-21 21:56:52 +0900

    유저 id 추가

--- src/main/java/com/sprint/mission/discodeit/DTO/Channel/CreatePublicChannelDTO.java
+++ src/main/java/com/sprint/mission/discodeit/DTO/Channel/CreatePublicChannelDTO.java
@@ -1,6 +1,8 @@
 package com.sprint.mission.discodeit.DTO.Channel;
 
+import java.util.UUID;
 public record CreatePublicChannelDTO(
+        UUID userId,
         String channelName,
         String description
 ) {
