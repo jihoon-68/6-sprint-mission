95128ace075ffc1094a1f60c58520ec66b030eb5
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-24 18:00:58 +0900

    파일 삭제, DTO 이름 변경

--- src/main/java/com/sprint/mission/discodeit/DTO/Message/MessageUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/DTO/Message/MessageUpdateRequest.java
@@ -3,7 +3,5 @@ import java.util.List;
 import java.util.UUID;
 
 public record UpdateMessageDTO(
-        UUID id,
-        String Content,
-        List<UUID> attachmentIds
+        String Content
 ) {}
