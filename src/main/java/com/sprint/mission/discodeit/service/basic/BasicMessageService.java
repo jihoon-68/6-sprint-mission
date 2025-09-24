57fa20b2865cfe92db6a9b7cd4e5d425cf8e1625
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-24 20:17:03 +0900

    더이상 메시지 업데이트에서 첨부 파일 제외 로 파일 로직 제거

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -1,11 +1,8 @@
 package com.sprint.mission.discodeit.service.basic;
 
-import com.sprint.mission.discodeit.DTO.BinaryContent.CreateBinaryContentDTO;
 import com.sprint.mission.discodeit.DTO.BinaryContent.FileDTO;
-import com.sprint.mission.discodeit.DTO.Channel.FindChannelDTO;
 import com.sprint.mission.discodeit.DTO.Message.CreateMessageDTO;
 import com.sprint.mission.discodeit.DTO.Message.FindMessageDTO;
-import com.sprint.mission.discodeit.DTO.Message.UpdateMessageDTO;
 import com.sprint.mission.discodeit.utilit.FileUpload;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.Message;
@@ -15,12 +12,10 @@ import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.MessageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.SchedulingException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.Path;
-import java.util.ArrayList;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
@@ -36,7 +31,7 @@ public class BasicMessageService implements MessageService {
 
     @Override
     public Message create(List<MultipartFile> multipartFiles, CreateMessageDTO createMessageDTO) {
-        if(!userRepository.existsById(createMessageDTO.userId())
+        if(!userRepository.existsById(createMessageDTO.authorId())
                 || !channelRepository.existsById(createMessageDTO.channelId())){
 
             throw new NoSuchElementException("Sender Or Receiver channels are mandatory");
@@ -53,15 +48,17 @@ public class BasicMessageService implements MessageService {
             List<UUID> attachmentIds = files.stream()
                             .map(path -> binaryContentRepository.save(
                                     new BinaryContent(
-                                            createMessageDTO.userId(),
-                                            createMessageDTO.channelId(),
-                                            path.savedName()
-                                    )).getId()
+                                            path.FileName(),
+                                            path.file().getFreeSpace(),
+                                            path.file().getClass().getTypeName(),
+                                            path.file().toString().getBytes(StandardCharsets.UTF_8)
+                                    ))
+                                    .getId()
                             ).toList();
 
 
             return messageRepository.save(new Message(
-                    createMessageDTO.userId(),
+                    createMessageDTO.authorId(),
                     createMessageDTO.channelId(),
                     createMessageDTO.content(),
                     attachmentIds
@@ -69,68 +66,33 @@ public class BasicMessageService implements MessageService {
         }
 
         return messageRepository.save(new Message(
-                createMessageDTO.userId(),
+                createMessageDTO.authorId(),
                 createMessageDTO.channelId(),
                 createMessageDTO.content()
         ));
     }
 
     @Override
-    public FindMessageDTO find(UUID id) {
-        Message message = messageRepository.findById(id).orElseThrow(() -> new  NoSuchElementException("Message not found"));
-        List<String> address = message.getAttachmentIds().stream()
-                .map(adders -> binaryContentRepository.findById(adders)
-                        .orElseThrow(() -> new NoSuchElementException("binaryContent not found"))
-                        .getFilePath()
-                )
-                .toList();
-        return new FindMessageDTO(message, address);
+    public Message find(UUID id) {
+        return messageRepository.findById(id).orElseThrow(() -> new  NoSuchElementException("Message not found"));
     }
 
     @Override
-    public List<FindMessageDTO> findAllByChannelId(UUID channelId) {
+    public List<Message> findAllByChannelId(UUID channelId) {
         List<Message> messages = messageRepository.findAll();
 
         return messages.stream()
-                .filter(message -> message.getChannel().equals(channelId))
-                .map(message -> {
-                    List<String> fileAddress = message.getAttachmentIds().stream()
-                            .map(address -> binaryContentRepository.findById(address)
-                                    .orElseThrow(()->new NoSuchElementException("binaryContent not found"))
-                                    .getFilePath()
-                            )
-                            .toList();
-                    return new FindMessageDTO(message,fileAddress);
-                })
+                .filter(message -> message.getChannelId().equals(channelId))
                 .toList();
     }
 
     @Override
-    public void update(List<MultipartFile> multipartFiles, UpdateMessageDTO updateMessageDTO) {
-        Message messageUpdated = messageRepository.findById(updateMessageDTO.id())
+    public Message update(UUID messageId, String newContent) {
+        Message messageUpdated = messageRepository.findById(messageId)
                 .orElseThrow(() -> new  NoSuchElementException("Message not found"));
-        if(!multipartFiles.isEmpty()){
-            List<FileDTO> files = fileUpload.upload(multipartFiles);
 
-            if(files.isEmpty()){
-                throw new NullPointerException("Multipart files is empty");
-            }
-
-            List<UUID> attachmentIds = new ArrayList<>(files.stream()
-                    .map(file -> binaryContentRepository.save(
-                            new BinaryContent(
-                                    messageUpdated.getSender(),
-                                    messageUpdated.getChannel(),
-                                    file.savedName()
-                            )
-                    ).getId())
-                    .toList());
-            attachmentIds.addAll(updateMessageDTO.attachmentIds());
-            messageUpdated.update(updateMessageDTO.Content(), attachmentIds);
-        }else{
-            messageUpdated.update(updateMessageDTO.Content(), new ArrayList<>());
-        }
-        messageRepository.save(messageUpdated);
+        messageUpdated.update(newContent);
+        return messageRepository.save(messageUpdated);
     }
 
     @Override
