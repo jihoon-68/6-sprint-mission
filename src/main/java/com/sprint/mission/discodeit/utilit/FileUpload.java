a6cfec42e07e4400aaf5b9898cd4379ce42cb419
jihoon-68 <pkrwlgns64@gmail.com>
2025-09-24 20:20:18 +0900

    임포트 정리

--- src/main/java/com/sprint/mission/discodeit/utilit/FileUpload.java
+++ src/main/java/com/sprint/mission/discodeit/utilit/FileUpload.java
@@ -3,8 +3,6 @@ package com.sprint.mission.discodeit.utilit;
 import com.sprint.mission.discodeit.DTO.BinaryContent.FileDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
