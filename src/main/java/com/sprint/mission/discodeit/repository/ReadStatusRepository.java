4926f102d333decfb77b93c19d59d7ac1f794813
jihoon-68 <pkrwlgns64@gmail.com>
2025-10-04 20:30:23 +0900

    JPA 적용

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -1,15 +1,13 @@
 package com.sprint.mission.discodeit.repository;
 
+import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.ReadStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ReadStatusRepository {
-    ReadStatus save(ReadStatus ReadStatus);
-    Optional<ReadStatus> findById(UUID id);
-    List<ReadStatus> findAll();
-    boolean existsById(UUID id);
-    void deleteById(UUID id);
+public interface ReadStatusRepository extends JpaRepository<ReadStatus, UUID> {
+
 }
--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -1,19 +1,12 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository {
-
-    //기존 CRUD
-    User save(User user);
-    Optional<User> findById(UUID id);
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
-    List<User> findAll();
-    boolean existsById(UUID id);
-    void deleteById(UUID id);
-
 }
