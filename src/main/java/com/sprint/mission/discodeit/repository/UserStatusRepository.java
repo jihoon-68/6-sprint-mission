aa0cc63380fc61116db74397b39a902f67c01b06
jihoon-68 <pkrwlgns64@gmail.com>
2025-10-04 20:30:23 +0900

    JPA 적용

--- src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.repository;
 
+import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
