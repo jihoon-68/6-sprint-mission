bc2c62a8f46aa71a197dc4b76a31fe7a6cf9244f
jihoon-68 <pkrwlgns64@gmail.com>
2025-10-10 23:56:04 +0900

    DTO 적극 도입하기로 DTO 추가

--- src/main/java/com/sprint/mission/discodeit/dto/BinaryContent/BinaryContentDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/BinaryContent/BinaryContentDto.java
@@ -8,4 +8,22 @@ public record BinaryContentDto (
         Long size,
         String contentType
 ){
+    public BinaryContentDto(UUID id, String fileName, Long size, String contentType){
+        this(
+                id,
+                fileName,
+                size,
+                contentType,
+                new byte[]{}
+        );
+    }
+    public BinaryContentDto(BinaryContentDto binaryContentDto,byte[] bytes){
+        this(
+                binaryContentDto.id(),
+                binaryContentDto.fileName(),
+                binaryContentDto.size(),
+                binaryContentDto.contentType(),
+                bytes
+        );
+    }
 }
