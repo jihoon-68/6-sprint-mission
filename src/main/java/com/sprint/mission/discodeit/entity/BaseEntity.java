be592b7669533fc537bf3ccb100ca877b5da16df
jihoon-68 <pkrwlgns64@gmail.com>
2025-10-11 02:51:25 +0900

    entity JPA 관계 수정,엔티티 등록 방법 수정

--- src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.UuidGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -14,13 +15,10 @@ import java.util.UUID;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
     @Id
+    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
     private UUID id;
 
-    @Column(nullable = false, updatable = false)
+    @Column(nullable = false)
     @CreatedDate
-    private Instant createdAt;
-
-    BaseEntity() {
-        id = UUID.randomUUID();
-    }
+    private Instant  createdAt;
 }
