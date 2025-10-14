88fd5303c7312bc1548290097846a4c5694140e4
jihoon-68 <pkrwlgns64@gmail.com>
2025-10-14 13:39:33 +0900

    바이트배열 분리로 레코드에 있는 다른 생성자 삭제

--- src/main/java/com/sprint/mission/discodeit/dto/BinaryContent/BinaryContentDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/BinaryContent/BinaryContentDto.java
@@ -8,22 +8,4 @@ public record BinaryContentDto (
         Long size,
         String contentType
 ){
-    public BinaryContentDto(UUID id, String fileName, Long size, String contentType){
-        this(
-                id,
-                fileName,
-                size,
-                contentType,
-                new byte[]{}
-        );
-    }
-    public BinaryContentDto(BinaryContentDto binaryContentDto,byte[] bytes){
-        this(
-                binaryContentDto.id(),
-                binaryContentDto.fileName(),
-                binaryContentDto.size(),
-                binaryContentDto.contentType(),
-                bytes
-        );
-    }
 }
