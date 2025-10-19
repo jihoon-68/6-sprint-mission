5ec7b766387b23d89c8e90c069936c1c0e8e3370
jihoon-68 <pkrwlgns64@gmail.com>
2025-10-19 20:00:21 +0900

    channel 맵펑에서  Repository 의존성 제거

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
 
