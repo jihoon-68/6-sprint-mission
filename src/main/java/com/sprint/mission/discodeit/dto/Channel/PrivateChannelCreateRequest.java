ac30fc1fd142f8af8b8092e3416a6cfaec27474e
jihoon-68 <pkrwlgns64@gmail.com>
2025-11-08 22:02:11 +0900

    feat: dto builder&validation 추가

--- src/main/java/com/sprint/mission/discodeit/dto/Channel/PrivateChannelCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/Channel/PrivateChannelCreateRequest.java
@@ -1,6 +1,5 @@
 package com.sprint.mission.discodeit.dto.Channel;
 
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 import java.util.List;
@@ -8,7 +7,6 @@ import java.util.UUID;
 
 @Builder
 public record PrivateChannelCreateRequest (
-        @NotEmpty(message = "유저는 입력은 필수 입니다")
         List<UUID> participantIds
 ) {
 }
--- src/main/java/com/sprint/mission/discodeit/dto/Message/MessageCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/Message/MessageCreateRequest.java
@@ -1,20 +1,13 @@
 package com.sprint.mission.discodeit.dto.Message;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.util.UUID;
 
 @Builder
 public record MessageCreateRequest (
-        @NotNull(message = "채널 정보는 필수입니다")
         UUID channelId,
-
-        @NotNull(message = "유저 정보는 필수입니다")
         UUID authorId,
-
-        @NotBlank(message = "메시지는 필수입니다")
         String content
 ){
 }
