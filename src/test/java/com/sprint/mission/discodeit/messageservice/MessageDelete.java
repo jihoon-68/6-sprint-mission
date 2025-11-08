bd367b43907e8154c214aa73ee333e5e0d89a7ac
jihoon-68 <pkrwlgns64@gmail.com>
2025-11-08 21:59:48 +0900

    feat: 메시지 단위 테스트 추가

--- src/test/java/com/sprint/mission/discodeit/messageservice/MessageDelete.java
+++ src/test/java/com/sprint/mission/discodeit/messageservice/MessageDelete.java
@@ -11,7 +11,6 @@ import com.sprint.mission.discodeit.exception.ErrorCode;
 import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.service.basic.BasicMessageService;
-import com.sprint.mission.discodeit.support.BinaryContentFixture;
 import com.sprint.mission.discodeit.support.ChannelFixture;
 import com.sprint.mission.discodeit.support.MessageFixture;
 import com.sprint.mission.discodeit.support.UserFixture;
@@ -54,7 +53,7 @@ public class MessageDelete {
                 .size(10L)
                 .contentType("txt")
                 .build();
-        BinaryContentFixture.setBinaryContentId(binaryContent,UUID.randomUUID());
+
         Message message = MessageFixture.createMessage(new User(),new Channel(),"테스트",List.of(binaryContent));
         MessageFixture.setMessageId(message,messageId);
 
