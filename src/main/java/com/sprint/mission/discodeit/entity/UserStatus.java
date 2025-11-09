09af085f11320475bb9e08471aa987c54dd9b027
jihoon-68 <pkrwlgns64@gmail.com>
2025-11-09 18:28:37 +0900

    feat: 슬라이스 테스트 추가로 @Builder,@Valid 등 추가 및 코드 정리

--- src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -1,10 +1,7 @@
 package com.sprint.mission.discodeit.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -25,6 +22,7 @@ public class UserStatus extends BaseUpdatableEntity {
     @Column(nullable = false)
     private Instant lastAccessAt;
 
+    @Builder
     public UserStatus(User user) {
         this.user = user;
         this.lastAccessAt = Instant.now();
