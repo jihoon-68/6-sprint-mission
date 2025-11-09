722a7447a34448aa97a6b0c5d05cf6d18b2cf9ba
jihoon-68 <pkrwlgns64@gmail.com>
2025-11-10 04:09:50 +0900

    머지중

--- src/main/java/com/sprint/mission/discodeit/dto/Message/MessageUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/Message/MessageUpdateRequest.java
@@ -3,6 +3,6 @@ package com.sprint.mission.discodeit.DTO.Message;
 import java.util.List;
 import java.util.UUID;
 
-public record UpdateMessageDTO(
+public record MessageUpdateRequest(
         String Content
 ) {}
