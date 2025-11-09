f5f6bbb2890481fadddbee3de82ee5438bc2e7d6
jihoon-68 <pkrwlgns64@gmail.com>
2025-11-09 18:21:13 +0900

    feat: 파일 변경

--- src/test/java/com/sprint/mission/discodeit/channelservice/ChannelRepositoryTest.java
+++ src/test/java/com/sprint/mission/discodeit/channelservice/ChannelRepositoryTest.java
@@ -1,20 +1,12 @@
 package com.sprint.mission.discodeit.channelservice;
 
 
-import com.sprint.mission.discodeit.repository.ChannelRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@DataJpaTest
-public class ChannelRepositoryTest {
+@DataJdbcTest
+public class ChannelRepository {
 
     @Autowired
     private ChannelRepository channelRepository;
-
-
-
-
 }
--- src/test/java/com/sprint/mission/discodeit/userservice/UserRepository.java
+++
@@ -1,4 +0,0 @@
-package com.sprint.mission.discodeit.userservice;
-
-public class UserRepository {
-}
