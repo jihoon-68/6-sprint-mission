96f9ce43439f4192c69b899290b668dd6f45cc1b
jihoon-68 <pkrwlgns64@gmail.com>
2025-11-10 03:22:29 +0900

    머지중

--- src/main/java/com/sprint/mission/discodeit/dto/User/UpdateUserDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/User/UpdateUserDto.java
@@ -1,39 +1,11 @@
 package com.sprint.mission.discodeit.dto.User;
 
-import com.sprint.mission.discodeit.entity.BinaryContent;
-import com.sprint.mission.discodeit.entity.UserStatus;
-
 import java.util.UUID;
 
 public record UpdateUserDto(
         UUID id,
-        String username,
+        String userName,
         String email,
-        BinaryContent profile,
-        String password,
-        UserStatus status
-) {
-
-    public static UpdateUserDto getStatus(UserStatus status) {
-        return new UpdateUserDto(
-                null,
-                null,
-                null,
-                null,
-               null,
-                status
-
-        );
-    }
-    public static UpdateUserDto getUpdateUser(UUID userId, UserUpdateRequest userUpdateRequest, BinaryContent binaryContent ) {
-        return new UpdateUserDto(
-                userId,
-                userUpdateRequest.newUsername(),
-                userUpdateRequest.newEmail(),
-                binaryContent,
-                userUpdateRequest.newPassword(),
-                null
-        );
-    }
-
-}
+        UUID profileId,
+        String password
+) {}
